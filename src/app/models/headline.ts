bc538de59238837563cc26988881ffb897a87ce1
David Rittmeyer <davidrittmeyer@Davids-MacBook-Pro.local>
2019-06-21 16:07:05 +0200

    Service erstellt, Modell erstellt, Grundgerüst für erste Seite

---
+++ src/app/models/headline.ts
@@ -0,0 +1,8 @@
+export class Headline {
+  title: string;
+  author: string;
+  source: string;
+  publicationDate: Date;
+  content: string;
+  constructor() {}
+}
--- src/app/show-headlines/show-headlines.component.ts
+++ src/app/show-headlines/show-headlines.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Headline} from '../models/headline';
+
+const ELEMENT_DATA: Headline[] = [
+  {title: 'testTitle', author: 'testAuthor', source: 'testSource', publicationDate: new Date('01/23/2000'), content: 'testContent'}
+];
 
 @Component({
   selector: 'app-show-headlines',
@@ -9,7 +14,10 @@ export class ShowHeadlinesComponent implements OnInit {
 
   constructor() { }
 
+  headlines: Headline[];
+
   ngOnInit() {
+    this.headlines = ELEMENT_DATA;
   }
 
 }
