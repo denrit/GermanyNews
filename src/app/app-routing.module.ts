85145f0a74f5dcf61db073126179e75ba553db17
David Rittmeyer <davidrittmeyer@Davids-MacBook-Pro.local>
2019-06-27 09:48:10 +0200

    implemented navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ShowHeadlinesComponent} from './show-headlines/show-headlines.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ShowHeadlinesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShowHeadlinesComponent } from './show-headlines/show-headlines.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShowHeadlinesComponent
+    ShowHeadlinesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
